977a20d83736fec687b620b225bceb62b9781f53
Alexandr250 <Alexandr250@bk.ru>
2020-02-12 19:20:36 +0300

    _

---
+++ Entites/Currency.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyQuotesCore.Entites {
+    [DebuggerDisplay( "{Designation} : {Name}" )]
+    public class Currency {
+        [JsonProperty( "ID" )]
+        public string Id { get; set; }
+
+        [JsonProperty( "NumCode" )]
+        public string Code { get; set; }
+
+        [JsonProperty( "CharCode" )]
+        public string Designation { get; set; }
+
+        [JsonProperty( "Nominal" )]
+        public int Nominal { get; set; }
+
+        [JsonProperty( "Name" )]
+        public string Name { get; set; }
+
+        [JsonProperty( "Value" )]
+        public double Value { get; set; }
+
+        [JsonProperty( "Previous" )]
+        public double PreviousValue { get; set; }
+    }
+}
---
+++ Entites/ExchangeRates.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyQuotesCore.Entites {
+    public class ExchangeRates {
+        [JsonProperty( "Date" )]
+        public DateTime CurrentDate { get; set; }
+
+        [JsonProperty( "PreviousDate" )]
+        public DateTime PreviousDate { get; set; }
+
+        [JsonProperty( "PreviousURL" )]
+        public string PreviousUrl { get; set; }
+
+        [JsonProperty( "Timestamp" )]
+        public DateTime Timestamp { get; set; }
+
+        [JsonProperty( "Valute" )]
+        public Dictionary<string, Currency> Currencies { get; set; }
+    }
+}
